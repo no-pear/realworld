587e7aa445221abd25954d7b57022f5e719fb003
hansxiang <xc980503@163.com>
2020-12-29 12:31:50 +0800

    change: homePage

--- pages/home/index.vue
+++ pages/home/index.vue
@@ -2,7 +2,7 @@
   <div class="home-page">
     <div class="banner">
       <div class="container">
-        <h1 class="logo-font">abcde</h1>
+        <h1 class="logo-font">万事终如意</h1>
         <p>A place to share your knowledge.</p>
       </div>
     </div>
