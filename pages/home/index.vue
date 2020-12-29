a157a97bb5031d13c2ffb2663ec6cf34b4b7b385
hansxiang <xc980503@163.com>
2020-12-29 14:06:00 +0800

    change: home

--- pages/home/index.vue
+++ pages/home/index.vue
@@ -2,7 +2,7 @@
   <div class="home-page">
     <div class="banner">
       <div class="container">
-        <h1 class="logo-font">万事终如意</h1>
+        <h1 class="logo-font">意难平终将和解</h1>
         <p>A place to share your knowledge.</p>
       </div>
     </div>
